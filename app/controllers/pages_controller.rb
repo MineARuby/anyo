5fad15ce6faa56b40581ecdd33c662b9ce68a86e
Etienne Lioret <kavatine999@gmail.com>
2021-06-02 15:44:00 +0200

    Added information on top of script that it is a bash script

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -40,7 +40,13 @@ class PagesController < ApplicationController
     str = "set depth 17\n"
     str += 'set variation "e2e4 e7e6 g1f3 b8c6"' + "\n"
     # exec("pwd")
-    fork { exec("bin/noexpect_script.exp") }
+    IO.pipe do |read_pipe, write_pipe|
+      exec("bin/noexpect_script.exp", out: write_pipe)
+      write_pipe.close
+      while line = read_pipe.gets
+        puts line
+      end
+    end
     # fork { exec("bin/basic_script3.exp") }
     # puts "pwd"
     # system("pwd")
@@ -62,7 +68,6 @@ class PagesController < ApplicationController
     # puts "ls"
     # system("ls")
     # fork { exec("bin/copied_script3.exp") }
-    # IO.pipe do |read_pipe, write_pipe|
     #   system("expect", "copied_script.exp", out: write_pipe) }
     #   write_pipe.close
     #   while line = read_pipe.gets
