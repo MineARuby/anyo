442e0e853dc317647d49861e80038db95642a959
Etienne Lioret <kavatine999@gmail.com>
2021-06-04 16:07:26 +0200

    Had completely forgotten to add the routes : fixed

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :depthevals, only: :index
       resources :mvtimes, only: :index
+      resources :fendepthevals, only: :index
+      resources :fenmvtimes, only: :index
     end
   end
       # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
